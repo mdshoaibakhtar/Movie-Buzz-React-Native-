04f61526184ee86d3e7589eb13e1a86d075d32cd
Md Shoaib <mdshoaibakhtar966@gmail.com>
2025-11-10 12:38:32 +0530

    test bad commit

--- app/index.tsx
+++ app/index.tsx
@@ -1,16 +1,14 @@
 import { Link } from "expo-router";
-import { View } from "react-native";
+import { Text, View } from "react-native";
+import { useUser } from "../src/hooks/useUser";
 
-
-export default function Index() {
+export default function Home() {
+  const name = useUser();
+  const a = "bad formatting"
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Home</Text>
+      <Text style={{ marginVertical: 20 }}>Stored Name: {name}</Text>
       <Link href="/form">Go to Form</Link>
     </View>
   );
