b09da93ab20280b011ea9a890f2547adc0d59ae9
Md Shoaib <mdshoaibakhtar966@gmail.com>
2025-11-10 14:39:13 +0530

    Code cleanup

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,31 @@
+import i18n from "@/src/i18n";
 import { Stack } from "expo-router";
 import { Provider } from "react-redux";
 import { store } from "../src/store";
 
 export default function RootLayout() {
+  console.log(i18n.language);
   return (
     <Provider store={store}>
-      <Stack />
+      <Stack>
+        <Stack.Screen
+          name="index"
+          options={{
+            title: "Home",
+            headerShown: true,
+            // headerRight: () => <LanguageDropdown />,
+          }}
+        />
+
+        <Stack.Screen
+          name="form/index"
+          options={{
+            title: "Form",
+            headerShown: true,
+            // headerRight: () => <LanguageDropdown />,
+          }}
+        />
+      </Stack>
     </Provider>
   );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -3,7 +3,7 @@ import { useLanguage } from "@/src/hooks/useLanguage";
 import { useProfile } from "@/src/hooks/useProfile";
 import { useRouter } from "expo-router";
 import { useTranslation } from "react-i18next";
-import { Button, Text, TextInput, View } from "react-native";
+import { Button, StyleSheet, Text, View } from "react-native";
 
 export default function Home() {
   const data = useFormData();
@@ -15,13 +15,13 @@ export default function Home() {
 
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>{t("welcome")}</Text>
-      <Button title="Hindi" onPress={() => changeLanguage("hi")} />
+      <Text style={styles.title}>{t("welcome")}</Text>
+      {/* <Button title="Hindi" onPress={() => changeLanguage("hi")} />
       <Button title="English" onPress={() => changeLanguage("en")} />
-      <Text style={{ marginVertical: 20 }}>Stored Name: {data.name}</Text>
+      <Text style={{ marginVertical: 20 }}>Stored Name: {data.name}</Text> */}
       <Button title="Go to Form" onPress={() => router.push("/form")} />
 
-      <Text>Profile Name</Text>
+      {/* <Text>Profile Name</Text>
 
       <TextInput
         value={name}
@@ -29,7 +29,15 @@ export default function Home() {
         style={{ borderWidth: 1, padding: 8, marginVertical: 10 }}
       />
 
-      <Text style={{ marginVertical: 20 }}>Profile Name: {name}</Text>
+      <Text style={{ marginVertical: 20 }}>Profile Name: {name}</Text> */}
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  title: {
+    justifyContent: "center",
+    alignItems: "center",
+    marginBottom: 12,
+  },
+});
